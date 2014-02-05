12f5d5dcccaacedf7e435eb061ea1319d8fc92a1
alexyang21 <alexyang.personal@gmail.com>
2014-02-04 20:31:25 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Test020314::Application.routes.draw do
+get 'pages/about'
+get 'pages/contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
