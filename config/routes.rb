90f1890e6754581d141aa17e3f24aa662d336ca8
alexyang21 <alexyang.personal@gmail.com>
2014-02-04 20:29:56 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Test020314::Application.routes.draw do
-root 'app/controllers/pages_controller.rb#about'
-get 'app/controllers/pages_controller.rb/about'
-get 'app/controllers/pages_controller.rb/contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
